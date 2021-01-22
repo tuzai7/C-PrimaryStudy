5513c768ecd471c154cf2fcf99cc7181b77d567f
BaldCub <1136646192@qq.com>
2021-01-22 21:36:55 +0800

    还没有完全弄懂 先传简单的

---
+++ text-1.22/text-1.22/源.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//函数调用
+//#include<stdio.h>
+//void newline()
+//{
+//	printf("*****\n");
+//}
+//void scendline()
+//{
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		newline();
+//	}
+//}
+//int main()
+//{
+//	scendline();
+//	return 0;
+//}
+
+//链式访问
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	/*int len = strlen("abcdef");
+//	printf("%d\n", len);*/
+//
+//	printf("%d\n", strlen("abcdef"));//链式访问
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d", 43)));
+//	return 0;
+//} //  输出结果为4321
+
+
+#include<stdiO.h>
+int main()
+{
+
+}
